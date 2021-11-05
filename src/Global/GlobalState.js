e95c17a247687ad9a6604f4e3ca40080a354317d
Leonardo Fernandes Martins <fmartins.leonardo@gmail.com>
2021-11-05 09:48:15 -0300

    perfil e loading

--- src/Global/GlobalState.js
+++ src/Global/GlobalState.js
@@ -1,42 +1,54 @@
-import React, {useState, useEffect} from 'react'
-import useRequestData from '../hooks/useRequestData'
-import { BASE_URL } from '../constants/urls'
-import GlobalStateContext from './GlobalStateContext'
-
+import React, { useState, useEffect } from "react";
+import useRequestData from "../hooks/useRequestData";
+import { BASE_URL } from "../constants/urls";
+import GlobalStateContext from "./GlobalStateContext";
+import InitialLoading from "../components/InitialLoading/InitialLoadingPage";
 const GlobalState = (props) => {
+  const getRestaurants = useRequestData([], `${BASE_URL}/restaurants`);
 
-  const getRestaurants = useRequestData([], `${BASE_URL}/restaurants`)
+  const [cart, setCart] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
+  const [appLoaded, setAppLoaded] = useState(false);
+  const [total, setTotal] = useState(0);
+  const [isClicked, setIsClicked] = useState(false);
+  const [isAmount, setIsAmount] = useState(false);
+  const [buyObject, setBuyObject] = useState();
+  const [restaurantId, setRestaurantId] = useState();
+  const [confirmBuy, setConfirmBuy] = useState({
+    products: [],
+    paymentMethod: "",
+  });
 
-  const [cart, setCart] = useState([])
-  const [restaurants, setRestaurants] = useState([])
-  const [total, setTotal] = useState(0)
-    const [isClicked, setIsClicked] = useState(false)
-    const [isAmount, setIsAmount] = useState(false)
-    const [buyObject, setBuyObject] = useState()
-    const [restaurantId, setRestaurantId] = useState()
-    const [confirmBuy, setConfirmBuy] = useState({
-        products: [],
-        paymentMethod: ''
-    })
+  useEffect(() => {
+    setRestaurants(getRestaurants.restaurants);
 
-    useEffect(() => {
-        setRestaurants(getRestaurants.restaurants)
-    }, [getRestaurants])
-
-    const data = {restaurantId, setRestaurantId, 
-      confirmBuy, setConfirmBuy, 
-      buyObject, setBuyObject, 
-      isAmount, setIsAmount, 
-      cart, setCart, 
-      restaurants, 
-      total, setTotal, 
-      isClicked, setIsClicked}
+    setTimeout(() => {
+      setAppLoaded(true);
+    }, 2000);
+  }, [getRestaurants]);
+  const data = {
+    restaurantId,
+    setRestaurantId,
+    confirmBuy,
+    setConfirmBuy,
+    buyObject,
+    setBuyObject,
+    isAmount,
+    setIsAmount,
+    cart,
+    setCart,
+    restaurants,
+    total,
+    setTotal,
+    isClicked,
+    setIsClicked,
+  };
 
   return (
-      <GlobalStateContext.Provider value={data}>
-          {props.children}
-      </GlobalStateContext.Provider>
-  )
-}
+    <GlobalStateContext.Provider value={data}>
+      {appLoaded ? props.children : <InitialLoading />}
+    </GlobalStateContext.Provider>
+  );
+};
 
-export default GlobalState
+export default GlobalState;
